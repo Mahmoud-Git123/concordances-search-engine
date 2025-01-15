b7f8f7a92892fc8942a8a63a4decf2cd84be0937
Mahmoud <mahmoudemail321@gmail.com>
2025-01-15 16:31:37 +0000

    Update xmlParsing.ts

--- src/lib/xmlParsing.ts
+++ src/lib/xmlParsing.ts
@@ -6,10 +6,10 @@ export const parseXML = async (xml: string) => {
 
   const conversations: {text: string; gender: string; age: number}[] = [];
 
-  result.conversation.u.forEach((utterance: any) => {
-    const text = utterance._; //  accesses the text of xml
-    const gender = utterance.$.gender; // accesses the attributes of text (i.e. gender and age)
-    const age = parseInt(utterance.$.age, 10); // parseInt to convert string to number and in base 10
+  result.conversation.u.forEach((u: any) => {
+    const text = u._; //  accesses the text of xml
+    const gender = u.$.gender; // accesses the attributes of text (i.e. gender and age)
+    const age = parseInt(u.$.age, 10); // parseInt to convert string to number and in base 10
 
     const conversationObj = {
         text: text,
@@ -23,9 +23,9 @@ export const parseXML = async (xml: string) => {
 
 const speakers = Array.from(
     new Set(
-      result.conversation.u.map((utterance: any) => ({
-        gender: utterance.$.gender, // accesses the attributes of text (i.e. gender and age)
-        age: parseInt(utterance.$.age, 10), // parseInt to convert string to number and in base 10
+      result.conversation.u.map((u: any) => ({
+        gender: u.$.gender, // accesses the attributes of text (i.e. gender and age)
+        age: parseInt(u.$.age, 10), // parseInt to convert string to number and in base 10
       }))
     )
   );
